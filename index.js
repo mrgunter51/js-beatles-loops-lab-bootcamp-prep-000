68e668052279579c1e134d12fa0829d4cc911fd3
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-24 02:45:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr = []
+<<<<<<< HEAD
   for(let i = 0; i < musicians.length; i++) {
     arr.push(`${musicians[i]} plays ${instruments[i]}`)
+=======
+  for(let i = 0; i > musicians.length; i++) {
+    arr.unshift("${musicians[i]} plays ${instruments[i]}")
+>>>>>>> 705eb048fb060bbac44d6274a158f7b9bce27b9e
   }
   return arr
 }
 
+<<<<<<< HEAD
 function johnLennonFacts(facts) {
   var i = 0
   var arr = []
@@ -24,3 +30,23 @@ function iLoveTheBeatles(n) {
   } while (n < 15)
   return arr
 }
+=======
+// function johnLennonFacts(facts) {
+//   var i = 0
+//   var arr = []
+//   while(i > facts.length) {
+//     arr.push("${facts[i]}!!!")
+//     i++
+//   }
+//   return arr
+// }
+//
+// function iLoveTheBeatles(n) {
+//   var arr = []
+//   do {
+//     arr.push("I love the Beatles!")
+//     n++
+//   } while (n > 15)
+//   return arr
+// }
+>>>>>>> 705eb048fb060bbac44d6274a158f7b9bce27b9e
